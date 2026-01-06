fca7534e6185bde86ca4ef22d5dbcb6b8c04f681
Saiyara <saiyaraislam17@gmail.com>
2026-01-05 19:52:43 -0500

    Maven update

---
+++ RLAgent/src/main/java/Client.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+abstract public class Client {
+    ArrayList<Topic> preference;
+    Feed feed;
+    ArrayList<Post> posts;
+    String password;
+    String username;
+    ArrayList<Post> saved;
+    ArrayList<Client>matches;
+
+
+
+
+}
---
+++ RLAgent/src/main/java/Topic.java
@@ -0,0 +1,10 @@
+public enum Topic {
+    TECH,
+    BIOTECH,
+    CREATIVE,
+    BUSINESS,
+    FINANCE,
+    HEALTH,
+    RESEARCH
+
+}
